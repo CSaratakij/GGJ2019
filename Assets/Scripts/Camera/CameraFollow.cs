141c2c1aaab24a4e8e376a6b9177fdc8f951acee
Chatchai Saratakij <CSaratakij@gmail.com>
2019-01-25 22:43:47 +0700

    Make player switch movement state when player is on the ground

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField]
+        Transform target;
+
+        [SerializeField]
+        float damp;
+
+        [SerializeField]
+        Vector3 offset;
+
+        Vector3 targetPosition;
+
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        void LateUpdate()
+        {
+            FollowTarget();
+        }
+
+        void Initialize()
+        {
+            if (target == null) {
+                Debug.Log("Can't find a follow target.");
+                return;
+            }
+        }
+
+        void FollowTarget()
+        {
+            targetPosition = Vector3.Lerp(transform.position, target.position + offset, damp);
+            targetPosition.z = -10.0f;
+            transform.position = targetPosition;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,135 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace GGJ
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        float gravity;
+
+        [SerializeField]
+        float moveForce;
+
+        [SerializeField]
+        float floatForce;
+
+        [SerializeField]
+        float maxFloatSpeed;
+
+        [SerializeField]
+        Transform ground;
+
+        [SerializeField]
+        Vector2 groundSize;
+
+        [SerializeField]
+        LayerMask groundMask;
+
+        [SerializeField]
+        LayerMask obstacleMask;
+
+        enum MovementState
+        {
+            Float,
+            Ground
+        }
+
+        MovementState movementState;
+
+        Vector2 inputVector;
+        Vector2 velocity;
+
+        Rigidbody2D rigid;
+        Collider2D groundCollider;
+
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            if (ground == null)
+                return;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(ground.position, groundSize);
+        }
+#endif
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        void Update()
+        {
+            InputHandler();
+        }
+
+        void FixedUpdate()
+        {
+            MovementHandler();
+            GroundCheckHandler();
+            ObstacleCheckHandler();
+        }
+
+        void Initialize()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void InputHandler()
+        {
+            inputVector.x = Input.GetAxisRaw("Horizontal");
+            inputVector.y = Input.GetAxisRaw("Vertical");
+        }
+
+        void MovementHandler()
+        {
+            switch (movementState)
+            {
+                case MovementState.Ground:
+                    MovementOnGround();
+                    break;
+
+                case MovementState.Float:
+                    MovementFloating();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        void GroundCheckHandler()
+        {
+            groundCollider = Physics2D.OverlapBox(ground.position, groundSize, 0.0f, groundMask);
+            movementState = (groundCollider == null) ? MovementState.Float : MovementState.Ground;
+        }
+
+        void ObstacleCheckHandler()
+        {
+
+        }
+
+        void MovementOnGround()
+        {
+            velocity.x = inputVector.x * moveForce;
+            velocity.y = -gravity;
+
+            rigid.velocity = velocity * Time.fixedDeltaTime;
+        }
+
+        void MovementFloating()
+        {
+            velocity.y = -gravity * Time.fixedDeltaTime;
+
+            velocity.x = inputVector.x * floatForce * Time.fixedDeltaTime;
+            velocity.x = Mathf.Clamp(velocity.x, -maxFloatSpeed, maxFloatSpeed);
+
+            rigid.AddForce(velocity, ForceMode2D.Force);
+        }
+    }
 }
