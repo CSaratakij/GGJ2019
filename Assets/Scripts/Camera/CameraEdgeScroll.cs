d416c132832376551bf40d6408425676bdaa16b3
Chatchai Saratakij <CSaratakij@gmail.com>
2019-01-27 02:48:48 +0700

    Make obstacle camera edge appear at the bottom of camera

---
+++ Assets/Scripts/Camera/CameraEdgeScroll.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ
+{
+    public class CameraEdgeScroll : MonoBehaviour
+    {
+        public static int PictureID = 0;
+
+        [SerializeField]
+        Vector3 viewportBound;
+
+        [SerializeField]
+        Vector3 offset;
+
+        [SerializeField]
+        SpriteRenderer spriteRenderer;
+
+        [SerializeField]
+        Sprite[] sprites;
+
+
+        bool isVisible = false;
+        Vector3 targetPos;
+
+        void Awake()
+        {
+            spriteRenderer.sprite = sprites[0];
+            SetVisible(true);
+        }
+
+        void LateUpdate()
+        {
+            if (isVisible) {
+                var screenBottomWorldPos = Camera.main.ViewportToWorldPoint(viewportBound);
+                targetPos = screenBottomWorldPos + offset;
+
+                targetPos.x = 0.0f;
+                targetPos.z = 0.0f;
+
+                transform.position = targetPos;
+            }
+        }
+
+        public void SetVisible(bool value)
+        {
+            isVisible = value;
+            spriteRenderer.sprite = sprites[PictureID];
+            spriteRenderer.enabled = value;
+        }
+    }
+}
