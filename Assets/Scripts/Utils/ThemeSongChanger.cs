49f97b106ec901396f5b3ec1cf080b62edc59d43
Chatchai Saratakij <CSaratakij@gmail.com>
2019-01-27 12:00:16 +0700

    Add theme song

---
+++ Assets/Scripts/Utils/ThemeSongChanger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace GGJ
+{
+    public class ThemeSongChanger : MonoBehaviour
+    {
+        [SerializeField]
+        bool isDisableOnExist;
+
+        [SerializeField]
+        AudioSource audioSource;
+
+        [SerializeField]
+        AudioClip enterSong;
+
+        [SerializeField]
+        AudioClip existSong;
+
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (audioSource == null)
+                return;
+
+            if (collision.CompareTag("Player")) {
+                audioSource.clip = enterSong;
+                audioSource.Play();
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            if (audioSource == null)
+                return;
+
+            if (collision.CompareTag("Player")) {
+                audioSource.clip = existSong;
+                audioSource.Play();
+
+                if (isDisableOnExist)
+                    gameObject.SetActive(false);
+            }
+        }
+
+    }
+}
